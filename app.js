9d6ad4a81e2ca05a4b3415eabe3f8b82284ad38a
humberto delgado <dehumberto@gmail.com>
2020-06-19 11:16:37 -0500

    Show forecast
    Api from https://openweathermap.org/

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const main = document.getElementsByClassName('main');
+const main = document.getElementsByClassName("main");
 
 const notes = [];
 
 // AÑADIR NOTAS
-const noteSubmit = document.getElementById('note-submit');
+const noteSubmit = document.getElementById("note-submit");
 
-function addNote(title = '', body, color, trash = false, pinned = false) {
-  const noteTitle = document.getElementById('note-title').value;
-  const noteBody = document.getElementById('note-body').value;
+function addNote(title = "", body, color, trash = false, pinned = false) {
+  const noteTitle = document.getElementById("note-title").value;
+  const noteBody = document.getElementById("note-body").value;
 
   const note = {
     title: noteTitle,
     body: noteBody,
-    color: 'white',
+    color: "white",
     trash: this.trash,
     pinned: this.pinned,
     updatedAt: Date.now(),
@@ -23,4 +23,56 @@ function addNote(title = '', body, color, trash = false, pinned = false) {
   console.log(notes);
 }
 
-noteSubmit.addEventListener('click', addNote);
+noteSubmit.addEventListener("click", addNote);
+
+function initialize() {
+  showForecast();
+}
+
+function showForecast() {
+  let forecastContainer = document.getElementById("forecast");
+
+  if (!navigator.geolocation) {
+    forecastContainer.innerHTML =
+      "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  async function success(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a805969695e94499b60e5a34386cd551&units=metric`
+    );
+    const data = await response.json();
+    const dataWeather = await data.message;
+    console.log(data);
+
+    const city = data.name;
+    const country = data.sys.country;
+    const temperature = data.main.temp;
+    const weather_description = data.weather[0].description;
+    // const weather_icon = data.weather[0].icon;
+
+    forecastContainer.innerHTML =
+      "<p>Welcome to {keepable}. Today's Forecast for " +
+      city +
+      ", " +
+      country +
+      ": " +
+      temperature +
+      "°, " +
+      weather_description.charAt(0).toUpperCase() +
+      weather_description.slice(1) +
+      "</p>";
+  }
+
+  function error() {
+    forecastContainer.innerHTML = "Unable to retrieve your location";
+  }
+
+  forecastContainer.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
